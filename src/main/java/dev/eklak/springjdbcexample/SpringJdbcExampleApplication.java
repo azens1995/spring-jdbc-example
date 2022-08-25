f623d9284e057bf313f29a9e240d452b0c672963
Eklak Dangaura <azens1995@gmail.com>
2022-08-25 11:34:53 +0545

    Initialize the project with required dependencies

---
+++ src/main/java/dev/eklak/springjdbcexample/SpringJdbcExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.eklak.springjdbcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/eklak/springjdbcexample/SpringJdbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eklak.springjdbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
